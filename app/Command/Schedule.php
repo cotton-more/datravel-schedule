d3ee68e3269df353203056cd9ded74e96a30814b
Ivan Nikulin <vansanblch@gmail.com>
2014-02-19 23:25:41 -0500

    show timetable

--- app/Command/Schedule.php
+++ app/Command/Schedule.php
@@ -8,35 +8,114 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+use DateTime;
+use DateInterval;
+
 class Schedule extends Command
 {
+    private $today;
+
     protected function configure()
     {
         $this->setName('schedule:mid-meeting');
         $this->setDescription('Generate timetable for meeting and testing days');
+        $this->addArgument(
+            'months',
+            InputArgument::OPTIONAL,
+            'Months to display',
+            6
+        );
     }
 
     protected function execute(InputInterface $in, OutputInterface $out)
     {
-        $lines = [];
-        // N 1 for Monday, 7 for Sunday
-        // t - number of days in the given month
+        $lines = [
+            [ 'Month', 'Meeting day', 'Testing day' ]
+        ];
 
         if (!ini_get('date.timezone')) {
             date_default_timezone_set('UTC');
         }
-        // $out->writeln(date_default_timezone_get());
-        // die();
 
-        $date = new \DateTime('2000-01-01');
+        $today = new DateTime();
+        $date = clone $today;
 
-        // get current day of the month and a month number
-        list($today, $month) = explode(' ', $date->format('j n'));
+        $this->today = $today;
 
-        $oneMonthInterval = new \DateInterval('P1M');
-        $i = 6;
+        $i = (int) $in->getArgument('months');
+        if ($i <= 0) {
+            throw new \Exception('Number of months should be greater than zero');
+        }
+
         do {
-            $date->add($oneMonthInterval);
+            $line = [
+                $date->format('F'),
+                $this->getMeetingDay($date),
+                $this->getTestingDay($date),
+            ];
+            $this->moveToNextMonth($date);
+            $lines[] = $line;
         } while (--$i);
+
+        foreach ($lines as $line) {
+            $out->writeln(implode(';', $line));
+        }
+    }
+
+    protected function getMeetingDay(DateTime $date)
+    {
+        $meetingDay = 14;
+        list($day, $month, $year) = explode(' ', $date->format('j n Y'));
+
+        $result = null;
+        if ($day <= $meetingDay) {
+            $date->setDate($year, $month, $meetingDay);
+            $weekday = (int) $date->format('N');
+            // if meeting day is Sat or Sun
+            // select following Mon
+            if ($weekday > 5) {
+                $offset = 8 - $weekday;
+                $date->modify('+'.$offset.' days');
+            }
+
+            $result = $date->format('j, D');
+        }
+
+        return $result;
+    }
+
+    protected function getTestingDay(DateTime $date)
+    {
+        $testingDay = $date->format('t');
+        list($day, $month, $year) = explode(' ', $date->format('j m Y'));
+        $date->setDate($year, $month, $testingDay);
+
+        $weekday = $date->format('N');
+        switch ($weekday) {
+            case 5:
+                $date->modify('-1 day');
+                break;
+            case 6:
+                $date->modify('-2 day');
+                break;
+            case 7:
+                $date->modify('-3 day');
+                break;
+            default:
+                break;
+        }
+
+        return $date->format('j, D');
+    }
+
+    protected function moveToNextMonth(DateTime $date)
+    {
+        static $oneMonthInterval = null;
+        if (null === $oneMonthInterval) {
+            $oneMonthInterval = new DateInterval('P1M');
+        }
+        $date->add($oneMonthInterval);
+        list($month, $year) = explode(' ', $date->format('n Y'));
+        $date->setDate($year, $month, 1);
     }
 }
